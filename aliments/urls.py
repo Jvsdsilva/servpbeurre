ce95226df56e742335a4849522af1dc04efa311c
Jota <mfjotaa@hotmail.com>
2020-02-21 10:59:37 +0100

    corr

--- aliments/urls.py
+++ aliments/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^index', views.index, name="index"),
-    url(r'^login/$', LoginView.as_view(template_name='aliments/login.html'), {'redirect_authenticated_user': True}, name='login'),
+    url(r'^login/$', LoginView.as_view(template_name='aliments/login.html'), name='login'),
     url(r'^results/$', views.results, name="results"),
     url(r'^results_details/(?P<pk>\d+)/$', views.results_details, name="results_details"),
     url(r'^aliment/', views.aliment, name="aliment"),
@@ -14,4 +14,5 @@ urlpatterns = [
     url(r'^contact/', views.index, name="contact"),
     url(r'^signup/', views.signup, name="signup"),
     url(r'^connected/', views.connected, name="connected"),
+    url(r'^connected/', LoginView.as_view(template_name='aliments/aliments.html'), name="connected"),
 ]
